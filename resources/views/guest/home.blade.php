54f89aa090980b4255524c450730b415dcfaca9e
585 <programmermh1@gmail.com>
2024-06-17 15:50:51 +0700

    -How to added

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -230,15 +230,14 @@
         </h1>
         <div class="row">
             <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-xs-12"  data-aos="fade-right" data-aos-delay="500">
-                <div class="card p-4">
+                <div class="card p-4" style="min-height: 176px">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 col-xs-6 position-relative">
                             <img src="{{url('guest/images/loan-request.png')}}" class="how-to-image">
                         </div>
                         <div class="col-xl-8 col-lg-7 col-md-6 col-sm-6 col-xs-6 ">
                             <h3 class="mb-0">1. Request Loan</h3>
-                            <p class="text-dark">Input your email address and needed loan amount and click submit button.<br/>
-                                And then wait response from administrator.
+                            <p class="text-dark mb-1">Fill in your details in the boxes provided on the website<br/>
                             </p>
                         </div>
                     </div>
@@ -247,15 +246,14 @@
             </div>
 
             <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-xs-12" data-aos="fade-down" data-aos-delay="700">
-                <div class="card p-4">
+                <div class="card p-4" style="min-height: 176px">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 col-xs-6 position-relative">
-                            <img src="{{url('guest/images/loan-request.png')}}" class="how-to-image">
+                            <img src="{{url('guest/images/keep-contact.png')}}" class="how-to-image">
                         </div>
                         <div class="col-xl-8 col-lg-7 col-md-6 col-sm-6 col-xs-6 ">
-                            <h3 class="mb-0">1. Request Loan</h3>
-                            <p class="text-dark">Input your email address and needed loan amount and click submit button.<br/>
-                                And then wait response from administrator.
+                            <h3 class="mb-0">2. Keep Contact</h3>
+                            <p class="text-dark">The loan's administrator will contact you and ask you to fill in an online application form
                             </p>
                         </div>
                     </div>
@@ -264,15 +262,14 @@
             </div>
 
             <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-xs-12" data-aos="fade-left" data-aos-delay="900">
-                <div class="card p-4">
+                <div class="card p-4" style="min-height: 176px">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 col-xs-6 position-relative">
-                            <img src="{{url('guest/images/loan-request.png')}}" class="how-to-image">
+                            <img src="{{url('guest/images/wait-money.png')}}" class="how-to-image">
                         </div>
                         <div class="col-xl-8 col-lg-7 col-md-6 col-sm-6 col-xs-6 ">
-                            <h3 class="mb-0">1. Request Loan</h3>
-                            <p class="text-dark">Input your email address and needed loan amount and click submit button.<br/>
-                                And then wait response from administrator.
+                            <h3 class="mb-0">3. Wait Money</h3>
+                            <p class="text-dark">You will receive a quick decision from our administrator and the funds will be made available to you.
                             </p>
                         </div>
                     </div>
